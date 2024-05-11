5bbe70df0102782dfc4086e822e17f664d9e508d
Kader ZKR <abdelkader.zekraoui1@gmail.com>
2024-05-11 20:27:46 +0100

    Updated the footer section endin with it

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -2,13 +2,17 @@
 import React from 'react'
 
 import classes from './index.module.scss'
-import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
 import { usePathname } from 'next/navigation'
 import { Gutter } from '../../Gutter'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Footer, Media } from '../../../../payload/payload-types'
+import { Button } from '../../Button'
 
-const FooterComponent = () => {
+const FooterComponent = ({ footer }: { footer: Footer }) => {
   const pathname = usePathname()
+  const navItems = footer?.navItems || []
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
       <Gutter>
@@ -28,7 +32,39 @@ const FooterComponent = () => {
           ))}
         </ul>
       </Gutter>
-      <div className={classes.footer}></div>
+      <div className={classes.footer}>
+        <Gutter>
+          <div className={classes.wrap}>
+            <Link href="/">
+              <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
+            </Link>
+
+            <p>{footer.copyright}</p>
+            <div className={classes.socialLinks}>
+              {navItems.map(item => {
+                const icon = item?.link?.icon as Media
+                return (
+                  <Button
+                    key={item.link.label}
+                    el="link"
+                    href={item.link.url}
+                    newTab={true}
+                    className={classes.socialLinkItem}
+                  >
+                    <Image
+                      src={icon?.url}
+                      alt={item.link.label}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    />
+                  </Button>
+                )
+              })}{' '}
+            </div>
+          </div>
+        </Gutter>
+      </div>
     </footer>
   )
 }
